ba4135d7b837b8251828ba159ca1855a0320ffe9
Tony Spiro <tspiro@tonyspiro.com>
2024-07-26 13:38:39 -0700

    edit: play button

--- cosmic/blocks/videos/PlayArea.tsx
+++ cosmic/blocks/videos/PlayArea.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { PlayIcon } from "lucide-react";
+
 import { VideoType } from "./VideoCard";
 import { useState } from "react";
 export function PlayArea({ video }: { video: VideoType }) {
@@ -16,7 +16,20 @@ export function PlayArea({ video }: { video: VideoType }) {
               className="aspect-video w-full object-cover"
             />
             <div className="absolute top-[50%] justify-center flex w-full">
-              <PlayIcon className="size-20 -mt-10" />
+              <svg
+                width="120"
+                height="120"
+                viewBox="0 0 120 120"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="-mt-[60px]"
+              >
+                <circle cx="60" cy="60" r="60" fill="white"></circle>
+                <path
+                  d="M86.0469 59.5619L46.2446 82.6355L46.1634 36.6289L86.0469 59.5619Z"
+                  fill="#191942"
+                ></path>
+              </svg>
             </div>
           </button>
         </div>
