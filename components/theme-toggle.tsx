343fc563a788a11c8718d9e47b5cb557c8fb2082
Tony Spiro <tspiro@tonyspiro.com>
2024-07-26 19:31:55 -0700

    edit: theme

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -4,20 +4,16 @@ import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/cosmic/elements/Button";
-
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
   return (
-    <Button
-      variant="ghost"
-      size="icon"
+    <button
+      className="p-2 flex flex-col justify-center"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="dark:hover:bg-black"
     >
-      <Sun className="h-[1.5rem] w-[1.3rem] text-black dark:text-white dark:hidden" />
-      <Moon className="hidden size-5 text-black dark:text-white dark:block" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+      <Sun className="mb-2 m-auto h-[1.5rem] w-[1.3rem] text-black dark:text-white dark:hidden" />
+      <Moon className="mb-2 m-auto hidden size-5  dark:block" />
+      <div className="text-sm m-auto">Theme</div>
+    </button>
   );
 }
