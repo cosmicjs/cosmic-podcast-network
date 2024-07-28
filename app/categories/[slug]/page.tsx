2576a4c078e126a0b5c296aca16b57a34b1c9761
Tony Spiro <tspiro@tonyspiro.com>
2024-07-27 18:29:31 -0700

    add: loader

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { VideoList } from "@/cosmic/blocks/videos/VideoList";
 import { CategoriesList } from "@/cosmic/blocks/videos/CategoriesList";
 import { cosmic } from "@/cosmic/client";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import { Loader } from "@/components/Loader";
 
 export default async function CategoryPage({
   params,
@@ -19,22 +21,26 @@ export default async function CategoryPage({
       .depth(1);
     return (
       <div className="p-4 md:px-8 mb-6">
-        <CategoriesList
-          query={{ type: "categories" }}
-          activeId={category.id}
-          limit={10}
-          skip={0}
-          className="mb-6 m-auto flex flex-wrap gap-2"
-        />
+        <Suspense fallback={<Loader />}>
+          <CategoriesList
+            query={{ type: "categories" }}
+            activeId={category.id}
+            limit={10}
+            skip={0}
+            className="mb-6 m-auto flex flex-wrap gap-2"
+          />
+        </Suspense>
         <h1 className="mb-6 text-3xl font-extrabold leading-tight tracking-tighter text-black dark:text-white md:text-4xl">
           {category.title}
         </h1>
-        <VideoList
-          query={{ type: "videos", "metadata.categories": category.id }}
-          limit={10}
-          skip={0}
-          className="m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
-        />
+        <Suspense fallback={<Loader />}>
+          <VideoList
+            query={{ type: "videos", "metadata.categories": category.id }}
+            limit={10}
+            skip={0}
+            className="m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+          />
+        </Suspense>
       </div>
     );
   } catch (e: any) {
