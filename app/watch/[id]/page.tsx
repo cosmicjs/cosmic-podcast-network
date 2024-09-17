4b5727663085e6e4e79fd58181a39ce3f96eaa13
Tony Spiro <tspiro@tonyspiro.com>
2024-09-17 08:57:51 -0700

    edit: revert cache test

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -2,7 +2,6 @@
 import { SingleVideo } from "@/cosmic/blocks/videos/SingleVideo";
 import { Loader } from "@/components/Loader";
 import { Suspense } from "react";
-import { cosmic } from "@/cosmic/client";
 
 export const revalidate = 3600;
 
@@ -16,15 +15,4 @@ export default async function SingleVideoPage({
       <SingleVideo query={{ id: params.id.split("-")[0], type: "videos" }} />
     </Suspense>
   );
-}
-
-export async function generateStaticParams() {
-  const { objects: videos } = await cosmic.objects
-    .find({ type: "videos" })
-    .props("id")
-    .limit(100)
-    .depth(0);
-  return videos.map((video: { id: string }) => ({
-    id: video.id,
-  }));
 }
--- cosmic/blocks/videos/SingleVideo.tsx
+++ cosmic/blocks/videos/SingleVideo.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import { cache } from "react";
 import { cosmic } from "@/cosmic/client";
 import { notFound } from "next/navigation";
 import { TimeAgo } from "@/components/TimeAgo";
@@ -8,35 +7,6 @@ import { PlayArea } from "./PlayArea";
 import Link from "next/link";
 import { CategoryPill, CategoryType } from "./CategoryPill";
 import { Comments } from "@/cosmic/blocks/comments/Comments";
-
-const fetchVideo = cache(
-  async (query: any, status?: "draft" | "published" | "any") => {
-    try {
-      const { object: video }: { object: VideoType } = await cosmic.objects
-        .findOne(query)
-        .props(
-          `{
-          id
-          slug
-          title
-          created_at
-          metadata
-        }`
-        )
-        .depth(1)
-        .status(status ? status : "published")
-        .options({
-          media: {
-            props: "alt_text",
-          },
-        });
-      return video;
-    } catch (e: any) {
-      if (e.status === 404) return null;
-      throw e;
-    }
-  }
-);
 
 export async function SingleVideo({
   query,
@@ -47,58 +17,77 @@ export async function SingleVideo({
   className?: string;
   status?: "draft" | "published" | "any";
 }) {
-  const video = await fetchVideo(query, status);
+  try {
+    const { object: video }: { object: VideoType } = await cosmic.objects
+      .findOne(query)
+      .props(
+        `{
+          id
+          slug
+          title
+          created_at
+          metadata
+        }`
+      )
+      .depth(1)
+      .status(status ? status : "published")
+      .options({
+        media: {
+          props: "alt_text",
+        },
+      });
 
-  if (!video) return notFound();
-
-  return (
-    <div className={className}>
-      <div className="w-full">
-        <PlayArea video={video} />
-      </div>
-      <section className="m-auto grid items-center pb-8 md:container p-4 pt-8 mb-8">
-        <div className="relative m-auto flex w-full md:w-[750px] flex-col items-start gap-2">
-          <h1 className="mb-4 text-3xl font-extrabold leading-tight tracking-tighter text-black dark:text-white md:text-4xl">
-            {video.title}
-          </h1>
-          <div className="mb-2 md:flex gap-4">
-            <Link
-              href={`/channels/${video.metadata.channel.slug}`}
-              className="flex items-center gap-3 mb-4"
-            >
-              <img
-                className="mr-2 h-[60px] w-[60px] rounded-full object-cover"
-                src={`${video.metadata.channel.metadata.thumbnail.imgix_url}?w=120&auto=format,compression`}
-                alt={video.metadata.channel.metadata.thumbnail.alt_text}
-              />
-              <div className="flex flex-col">
-                <span className="font-semibold text-zinc-800 dark:text-zinc-200">
-                  {video.metadata.channel.title}
-                </span>
-                <span className="text-zinc-500 dark:text-zinc-400">
-                  <TimeAgo time={video.created_at} />
-                </span>
-              </div>
-            </Link>
-            <div className="md:absolute md:right-0 flex">
-              {video.metadata.categories.map((category: CategoryType) => {
-                return <CategoryPill key={category.id} category={category} />;
-              })}
-            </div>
-          </div>
-          <div
-            className="space-y-4 text-zinc-700 dark:text-zinc-300"
-            dangerouslySetInnerHTML={{ __html: video.metadata.description }}
-          />
+    return (
+      <div className={className}>
+        <div className="w-full">
+          <PlayArea video={video} />
         </div>
-      </section>
-      <Comments
-        query={{
-          type: "comments",
-          "metadata.resource": video.id,
-          "metadata.approved": true,
-        }}
-      />
-    </div>
-  );
+        <section className="m-auto grid items-center pb-8 md:container p-4 pt-8 mb-8">
+          <div className="relative m-auto flex w-full md:w-[750px] flex-col items-start gap-2">
+            <h1 className="mb-4 text-3xl font-extrabold leading-tight tracking-tighter text-black dark:text-white md:text-4xl">
+              {video.title}
+            </h1>
+            <div className="mb-2 md:flex gap-4">
+              <Link
+                href={`/channels/${video.metadata.channel.slug}`}
+                className="flex items-center gap-3 mb-4"
+              >
+                <img
+                  className="mr-2 h-[60px] w-[60px] rounded-full object-cover"
+                  src={`${video.metadata.channel.metadata.thumbnail.imgix_url}?w=120&auto=format,compression`}
+                  alt={video.metadata.channel.metadata.thumbnail.alt_text}
+                />
+                <div className="flex flex-col">
+                  <span className="font-semibold text-zinc-800 dark:text-zinc-200">
+                    {video.metadata.channel.title}
+                  </span>
+                  <span className="text-zinc-500 dark:text-zinc-400">
+                    <TimeAgo time={video.created_at} />
+                  </span>
+                </div>
+              </Link>
+              <div className="md:absolute md:right-0 flex">
+                {video.metadata.categories.map((category: CategoryType) => {
+                  return <CategoryPill key={category.id} category={category} />;
+                })}
+              </div>
+            </div>
+            <div
+              className="space-y-4 text-zinc-700 dark:text-zinc-300"
+              dangerouslySetInnerHTML={{ __html: video.metadata.description }}
+            />
+          </div>
+        </section>
+        <Comments
+          query={{
+            type: "comments",
+            "metadata.resource": video.id,
+            "metadata.approved": true,
+          }}
+        />
+      </div>
+    );
+  } catch (e: any) {
+    if (e.status === 404) return notFound();
+  }
 }
