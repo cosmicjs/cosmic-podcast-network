9366cb2ce77eb76e80aded91e2fc6483ae27b13a
Tony Spiro <tspiro@tonyspiro.com>
2024-11-13 19:56:38 -0800

    edit: mobile update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,26 +1,56 @@
-import { HomeIcon, RssIcon } from "lucide-react";
+"use client";
+
+import { HomeIcon, RssIcon, MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { useState } from "react";
+import { cn } from "@/cosmic/utils";
 
 export function SideBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="w-[80px] hidden md:flex space-y-6 fixed h-screen border-r dark:border-gray-800 top-[70px] left-0 pt-6 flex-col justify-start">
-      <Link href="/" className="space-y-2 w-full flex flex-col justify-center">
-        <HomeIcon className="size-6 m-auto" />
-        <div className="m-auto text-sm">Home</div>
-      </Link>
-
-      <Link
-        href="/feed"
-        className="space-y-2 w-full flex flex-col justify-center"
+    <>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={cn(
+          "md:hidden border dark:border-gray-900 block md:bg-transparent bg-white shadow-sm dark:bg-gray-900 fixed left-0 top-20 text-primary-foreground p-2 rounded-r-lg z-50",
+          isOpen && "translate-x-[80px]",
+          "transition-transform duration-300"
+        )}
       >
-        <RssIcon className="size-6 m-auto" />
-        <div className="m-auto text-sm">My Feed</div>
-      </Link>
+        <MenuIcon className="size-6" />
+      </button>
 
-      <div className="m-auto">
-        <ThemeToggle />
+      <div
+        className={`
+        bg-white dark:bg-gray-900 md:dark:bg-transparent z-50 w-[80px] flex space-y-6 fixed h-screen border-r dark:border-gray-900 top-[70px] left-0 pt-6 flex-col justify-start
+        transition-transform duration-300
+        ${isOpen ? "translate-x-0" : "-translate-x-full"}
+        md:translate-x-0 md:block
+        bg-background
+      `}
+      >
+        <Link
+          href="/"
+          className="space-y-2 w-full flex flex-col justify-center"
+        >
+          <HomeIcon className="size-6 m-auto" />
+          <div className="m-auto text-sm">Home</div>
+        </Link>
+
+        <Link
+          href="/feed"
+          className="space-y-2 w-full flex flex-col justify-center"
+        >
+          <RssIcon className="size-6 m-auto" />
+          <div className="m-auto text-sm">My Feed</div>
+        </Link>
+
+        <div className="m-auto pl-2">
+          <ThemeToggle />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- cosmic/elements/TextArea.tsx
+++ cosmic/elements/TextArea.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/cosmic/utils"
+import { cn } from "@/cosmic/utils";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
@@ -10,15 +10,15 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+          "flex min-h-[80px] w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-base ring-offset-white placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
           className
         )}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Textarea.displayName = "Textarea"
+);
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
