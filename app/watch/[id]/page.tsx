06dda85c0577a7384e795f227298208f768ee307
Tony Spiro <tspiro@tonyspiro.com>
2024-11-22 17:26:59 -0800

    edit: generateStaticParams

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -2,6 +2,18 @@
 import { SingleVideo } from "@/cosmic/blocks/videos/SingleVideo";
 import { Loader } from "@/components/Loader";
 import { Suspense } from "react";
+import { cosmic } from "@/cosmic/client";
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  const { objects: videos } = await cosmic.objects.find({
+    type: "videos",
+  });
+  return videos.map((video: { id: string }) => ({
+    id: video.id,
+  }));
+}
 
 export default async function SingleVideoPage({
   params,
